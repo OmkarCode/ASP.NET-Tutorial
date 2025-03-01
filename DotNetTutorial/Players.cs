9285669e3dd62b6f2cf6b5489bce4035fff29d3f
OmkarCode <chauhanomkar2002@gmail.com>
2025-03-01 15:14:15 +0530

    ASP.NET Tutorial
    ASP.NET Tutorial

---
+++ DotNetTutorial/Players.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTutorial
+{
+    class Players
+    {
+        public String name = "Kashyap";
+        private int age = 19;
+
+        public int getAge()
+        {
+            return age;
+        }
+        public void setAge(int a)
+        {
+            age = a;
+        }
+    }
+
+}
