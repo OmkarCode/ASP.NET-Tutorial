11b71c5330f503364397945b146f6d18683fec42
OmkarCode <chauhanomkar2002@gmail.com>
2025-03-01 15:29:21 +0530

    ASP.NET Tutorial
    ASP.NET Tutorial, Basic Concept

---
+++ BasicConcepts/BasicConcepts/Controllers/AdditionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicConcepts.Controllers
+{
+    public class AdditionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection obj)
+        {
+            int num1 = Convert.ToInt32(obj["txtnum1"].ToString());
+            int num2 = Convert.ToInt32(obj["txtnum2"].ToString());
+            int num3 = num1 + num2;  
+
+            ViewBag.data = "Result is " + num3;
+            return View();
+        }
+    }
+}
---
+++ BasicConcepts/BasicConcepts/Controllers/SIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BasicConcepts.Models;
+
+namespace BasicConcepts.Controllers
+{
+    public class SIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(SI obj)
+        {
+            ViewBag.result = "SI is " + (obj.P * obj.R * obj.T) / 100 ;
+            return View();
+            
+        }
+    }
+}
